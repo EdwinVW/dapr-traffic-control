ba5db498c203e1f96f89be75d9952cfab81e6e6c
Edwin van Wijk <edwinvw@gmail.com>
2020-09-21 09:23:17 +0200

    Add API key requirement for RDW

--- src/GovernmentService/Controllers/RDWController.cs
+++ src/GovernmentService/Controllers/RDWController.cs
@@ -8,6 +8,7 @@ namespace GovernmentService.Controllers
     [ApiController]
     public class RDWController : ControllerBase
     {
+        private const string SUPER_SECRET_API_KEY = "A6k9D42L061Fx4Rm2K8";
         private readonly ILogger<RDWController> _logger;
         private readonly IVehicleInfoRepository _vehicleInfoRepository;
 
@@ -17,9 +18,14 @@ namespace GovernmentService.Controllers
             _vehicleInfoRepository = vehicleInfoRepository;
         }
 
-        [HttpGet("rdw/vehicle/{licenseNumber}")]
-        public ActionResult<VehicleInfo> GetVehicleDetails(string licenseNumber)
+        [HttpGet("rdw/{apikey}/vehicle/{licenseNumber}")]
+        public ActionResult<VehicleInfo> GetVehicleDetails(string apiKey, string licenseNumber)
         {
+            if (apiKey != SUPER_SECRET_API_KEY)
+            {
+                return Unauthorized();
+            }
+
             _logger.LogInformation($"RDW: Retrieving vehicle-info for licensenumber {licenseNumber}");
             VehicleInfo info = _vehicleInfoRepository.GetVehicleInfo(licenseNumber);
             return info;
