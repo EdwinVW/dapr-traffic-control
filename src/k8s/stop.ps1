b9787cd6b4700842b1bed7ef6dd80951d3521de7
EdwinVW <edwinvw@gmail.com>
2021-01-13 06:59:06 +0100

    Add Zipkin

--- src/k8s/stop.ps1
+++ src/k8s/stop.ps1
@@ -1,8 +1,9 @@
 kubectl delete `
-    -f ./simulation.yaml `
-    -f ./governmentservice.yaml `
-    -f ./trafficcontrolservice.yaml `
-    -f ./state-redis.yaml `
-    -f ./pubsub-redis.yaml `
-    -f ./secret.yaml `
-    -f ./namespace.yaml
+    -f simulation.yaml `
+    -f governmentservice.yaml `
+    -f trafficcontrolservice.yaml `
+    -f state-redis.yaml `
+    -f pubsub-redis.yaml `
+    -f secret.yaml `
+    -f zipkin.yaml `
+    -f dapr-config.yaml
