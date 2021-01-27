d32fc04e280908ded2f6e8ec67796bf13e51e191
EdwinVW <edwinvw@gmail.com>
2021-01-27 19:49:39 +0100

    Update Redis installation

--- src/k8s/install-redis.ps1
+++ src/k8s/install-redis.ps1
@@ -1,7 +1,4 @@
+kubectl apply -f namespace.yaml
+
 helm repo add bitnami https://charts.bitnami.com/bitnami
-helm install redis bitnami/redis
-kubectl get secret --namespace default redis -o jsonpath="{.data.redis-password}" > ./encoded.b64
-certutil -decode ./encoded.b64 ./password.txt > $null
-Get-Content ./password.txt
-Remove-Item ./encoded.b64
-Remove-Item ./password.txt
+helm install redis --set password=JOhCGe6Eau bitnami/redis
