c14fc402a19430e5cce7b06702dcef1b8c1dee5a
EdwinVW <edwinvw@gmail.com>
2021-01-27 19:58:44 +0100

    Update Redis installation

--- src/k8s/install-redis.ps1
+++ src/k8s/install-redis.ps1
@@ -1,4 +1,4 @@
 kubectl apply -f namespace.yaml
 
 helm repo add bitnami https://charts.bitnami.com/bitnami
-helm install redis --set password=JOhCGe6Eau bitnami/redis
+helm install redis bitnami/redis
