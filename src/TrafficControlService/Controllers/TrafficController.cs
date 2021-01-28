14d0506ff36a806809fcfa9416cff9750f844b0d
EdwinVW <edwinvw@gmail.com>
2021-01-28 09:16:18 +0100

    Add message retries to TrafficControlService

--- src/TrafficControlService/Controllers/TrafficController.cs
+++ src/TrafficControlService/Controllers/TrafficController.cs
@@ -32,26 +32,33 @@ namespace TrafficControlService.Controllers
         [HttpPost("entrycam")]
         public async Task<ActionResult> VehicleEntry(VehicleRegistered msg, [FromServices] DaprClient daprClient)
         {
-            var vehicleInfo = await daprClient.InvokeMethodAsync<VehicleInfo>(
-                "governmentservice",
-                $"rdw/vehicle/{msg.LicenseNumber}",
-                new HttpInvocationOptions { Method = HttpMethod.Get });
-
-            // log entry
-            _logger.LogInformation($"ENTRY detected in lane {msg.Lane} at {msg.Timestamp.ToString("hh:mm:ss")}: " +
-                $"{vehicleInfo.Brand} {vehicleInfo.Model} with license-number {msg.LicenseNumber}.");
-
-            // store vehicle state
-            var vehicleState = new VehicleState
+            try
             {
-                LicenseNumber = msg.LicenseNumber,
-                Brand = vehicleInfo.Brand,
-                Model = vehicleInfo.Model,
-                EntryTimestamp = msg.Timestamp
-            };
-            await daprClient.SaveStateAsync<VehicleState>(DAPR_STORE_NAME, msg.LicenseNumber, vehicleState);
+                var vehicleInfo = await daprClient.InvokeMethodAsync<VehicleInfo>(
+                    "governmentservice",
+                    $"rdw/vehicle/{msg.LicenseNumber}",
+                    HttpInvocationOptions.UsingGet());
 
-            return Ok();
+                // log entry
+                _logger.LogInformation($"ENTRY detected in lane {msg.Lane} at {msg.Timestamp.ToString("hh:mm:ss")}: " +
+                    $"{vehicleInfo.Brand} {vehicleInfo.Model} with license-number {msg.LicenseNumber}.");
+
+                // store vehicle state
+                var vehicleState = new VehicleState
+                {
+                    LicenseNumber = msg.LicenseNumber,
+                    Brand = vehicleInfo.Brand,
+                    Model = vehicleInfo.Model,
+                    EntryTimestamp = msg.Timestamp
+                };
+                await daprClient.SaveStateAsync<VehicleState>(DAPR_STORE_NAME, msg.LicenseNumber, vehicleState);
+
+                return Ok();
+            }
+            catch
+            {
+                return Ok(new { status = "RETRY" });
+            }
         }
 
         [Topic("pubsub", "trafficcontrol.exitcam")]
@@ -61,39 +68,46 @@ namespace TrafficControlService.Controllers
             //[FromState(DAPR_STORE_NAME)]StateEntry<VehicleInfo> state,
             [FromServices] DaprClient daprClient)
         {
-            // get vehicle state
-            var state = await daprClient.GetStateEntryAsync<VehicleState>(DAPR_STORE_NAME, msg.LicenseNumber);
-            if (state.Value == null)
+            try
             {
-                return NotFound();
-            }
-
-            // log exit
-            _logger.LogInformation($"EXIT detected in lane {msg.Lane} at {msg.Timestamp.ToString("hh:mm:ss")}: " +
-                $"{state.Value.Brand} {state.Value.Model} with license-number {msg.LicenseNumber}.");
-
-            // update state
-            state.Value.ExitTimestamp = msg.Timestamp;
-            await state.SaveAsync();
-
-            // handle possible speeding violation
-            int violation = _speedingViolationCalculator.DetermineSpeedingViolationInKmh(state.Value.EntryTimestamp, state.Value.ExitTimestamp);
-            if (violation > 0)
-            {
-                _logger.LogInformation($"Speeding violation detected ({violation} KMh) of {state.Value.Brand} {state.Value.Model} " +
-                    $"with license-number {state.Value.LicenseNumber}.");
-
-                var @event = new SpeedingViolationDetected
+                // get vehicle state
+                var state = await daprClient.GetStateEntryAsync<VehicleState>(DAPR_STORE_NAME, msg.LicenseNumber);
+                if (state.Value == null)
                 {
-                    VehicleId = msg.LicenseNumber,
-                    RoadId = _roadId,
-                    ViolationInKmh = violation,
-                    Timestamp = msg.Timestamp
-                };
-                await daprClient.PublishEventAsync<SpeedingViolationDetected>("pubsub", "cjib.speedingviolation", @event);
-            }
+                    return NotFound();
+                }
 
-            return Ok();
+                // log exit
+                _logger.LogInformation($"EXIT detected in lane {msg.Lane} at {msg.Timestamp.ToString("hh:mm:ss")}: " +
+                    $"{state.Value.Brand} {state.Value.Model} with license-number {msg.LicenseNumber}.");
+
+                // update state
+                state.Value.ExitTimestamp = msg.Timestamp;
+                await state.SaveAsync();
+
+                // handle possible speeding violation
+                int violation = _speedingViolationCalculator.DetermineSpeedingViolationInKmh(state.Value.EntryTimestamp, state.Value.ExitTimestamp);
+                if (violation > 0)
+                {
+                    _logger.LogInformation($"Speeding violation detected ({violation} KMh) of {state.Value.Brand} {state.Value.Model} " +
+                        $"with license-number {state.Value.LicenseNumber}.");
+
+                    var @event = new SpeedingViolationDetected
+                    {
+                        VehicleId = msg.LicenseNumber,
+                        RoadId = _roadId,
+                        ViolationInKmh = violation,
+                        Timestamp = msg.Timestamp
+                    };
+                    await daprClient.PublishEventAsync<SpeedingViolationDetected>("pubsub", "cjib.speedingviolation", @event);
+                }
+
+                return Ok();
+            }
+            catch
+            {
+                return Ok(new { status = "RETRY" });
+            }
         }
     }
 }
