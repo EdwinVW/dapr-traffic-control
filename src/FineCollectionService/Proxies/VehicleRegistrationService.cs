fa8722ed68782af704de16f4bf34f09bf30a15ed
EdwinVW <edwinvw@gmail.com>
2021-02-27 19:02:08 +0100

    Remove obsolete code

--- src/FineCollectionService/Proxies/VehicleRegistrationService.cs
+++ src/FineCollectionService/Proxies/VehicleRegistrationService.cs
@@ -9,22 +9,16 @@ namespace FineCollectionService.Proxies
     public class VehicleRegistrationService
     {
         private HttpClient _httpClient;
-        private JsonSerializerOptions _serializerOptions;
 
         public VehicleRegistrationService(HttpClient httpClient)
         {
             _httpClient = httpClient;
-            _serializerOptions = new JsonSerializerOptions
-            {
-                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
-                PropertyNameCaseInsensitive = true
-            };
         }
 
         public async Task<VehicleInfo> GetVehicleInfo(string licenseNumber)
         {
             return await _httpClient.GetFromJsonAsync<VehicleInfo>(
-                $"vehicleinfo/{licenseNumber}", _serializerOptions);
-        }       
+                $"vehicleinfo/{licenseNumber}");
+        }
     }
 }
