06556e758f9a10bff74148519d7992c14f6eb221
EdwinVW <edwinvw@gmail.com>
2021-10-19 16:08:20 +0200

    Update grafana scripts

--- src/Observability/grafana/get-grafana-password.ps1
+++ src/Observability/grafana/get-grafana-password.ps1
@@ -1,7 +1,3 @@
 # display Grafana admin password
-kubectl get secret --namespace dapr-monitoring grafana -o jsonpath="{.data.admin-password}" > ./encoded.b64
-certutil -decode ./encoded.b64 ./password.txt > $null
-certutil -decode ./encoded.b64 ./password.txt > $null
-Get-Content ./password.txt
-Remove-Item ./encoded.b64
-Remove-Item ./password.txt
+ $password = kubectl get secret --namespace dapr-monitoring grafana -o jsonpath="{.data.admin-password}"
+ [Text.Encoding]::Utf8.GetString([Convert]::FromBase64String($password))
