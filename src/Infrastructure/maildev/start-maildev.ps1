86feb6d3010c6f9d724334cb4434f7bd2909ae47
EdwinVW <edwinvw@gmail.com>
2022-07-14 11:51:07 +0200

    Update maildev version

--- src/Infrastructure/maildev/start-maildev.ps1
+++ src/Infrastructure/maildev/start-maildev.ps1
@@ -1,2 +1,2 @@
 # using older version of maildev (newer version did not render HTML mail correctly)
-docker run -d -p 4000:80 -p 4025:25 --name dtc-maildev maildev/maildev:latest
+docker run -d -p 4000:80 -p 4025:25 --name dtc-maildev maildev/maildev:1.1.0
