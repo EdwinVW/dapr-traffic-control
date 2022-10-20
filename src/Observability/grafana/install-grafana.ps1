7a189e66e13c2edc26aff8fd1ace7870a4be54c8
EdwinVW <edwinvw@gmail.com>
2022-10-20 15:34:11 +0300

    Update observability install script

--- src/Observability/grafana/install-grafana.ps1
+++ src/Observability/grafana/install-grafana.ps1
@@ -1,10 +1,15 @@
 kubectl apply -f namespace.yaml
 
+# Get/update helm charts
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm repo add grafana https://grafana.github.io/helm-charts
 helm repo update
+
+# Install Prometheus
+helm install dapr-prom prometheus-community/prometheus -n dapr-monitoring --set nodeExporter.hostRootfs=false
+
+# Install Grafana
 helm install grafana grafana/grafana -n dapr-monitoring 
 
-#--set persistence.enabled=false
-
-# display Grafana admin password
+# Display Grafana admin password
 & ./get-grafana-password.ps1
--- src/Observability/grafana/install-prometheus.ps1
+++
@@ -1,7 +0,0 @@
-kubectl apply -f namespace.yaml
-
-helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
-helm repo update
-helm install dapr-prom prometheus-community/prometheus -n dapr-monitoring 
-
-#--set alertmanager.persistentVolume.enable=false --set pushgateway.persistentVolume.enabled=false --set server.persistentVolume.enabled=false
